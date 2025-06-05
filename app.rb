be86280340e026b5fd33bedc25e072258140d4cd
plaschinsky <gplaschi@chicagobooth.edu>
2025-06-05 03:26:01 +0000

    index done

--- app.rb
+++ app.rb
@@ -2,8 +2,21 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb :index
+end
+
+get("/process_roll") do
+  @num_dice = 2
+
+  @sides = 4
+
+  @rolls = []
+
+  @num_dice.times do
+    die = rand(1..@sides)
+
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
 end
